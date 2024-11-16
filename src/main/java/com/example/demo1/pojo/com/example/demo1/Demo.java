0996c09f1fef3fa3d5eadc58be921bb58fa61032
uuufa <111222uuufa@gmail.com>
2024-11-16 21:21:43 +0800

    基本框架, 可以读数据库了

---
+++ src/main/java/com/example/demo1/pojo/com/example/demo1/Demo.java
@@ -0,0 +1,29 @@
+package com.example.demo1.pojo.com.example.demo1;
+
+import com.example.demo1.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Demo {
+
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        List<User> Users = sqlSession.selectList("test.select_all_user");
+        System.out.println(Users);
+
+
+        sqlSession.close();
+    }
+}
