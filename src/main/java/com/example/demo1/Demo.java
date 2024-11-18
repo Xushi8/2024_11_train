0d4dd1900b377dc23d0c4dcf0fd10dd968119f05
uuufa <111222uuufa@gmail.com>
2024-11-18 19:14:18 +0800

    user 增删改查 over

--- src/main/java/com/example/demo1/Demo.java
+++ src/main/java/com/example/demo1/Demo.java
@@ -30,17 +30,38 @@ public class Demo {
 //        List<User> Users = sqlSession.selectList("test.select_all_user");
 //        System.out.println(Users);
 
-
+        // user test
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         List<User> users = userMapper.select_all_users();
         System.out.println(users);
-        User user = userMapper.select_user_by_id(1);
+        User user = userMapper.select_user_by_ID(1);
         System.out.println(user);
         User user1 = userMapper.select_user(user);
         System.out.println(user1);
         User user2 = userMapper.select_user_by_Name("User B1");
         System.out.println(user2);
+        User user3 = userMapper.select_user_by_Name_and_Password("User B2", "password123");
+        System.out.println(user3);
+        User user4 = new User();
+        user4.setName("insert");
+        user4.setPassword("password123");
+        user4.setRole(2);
+        user4.setTenantName("Tenant A");
+        int res = userMapper.insert_user(user4);
+        System.out.println("增加的数量: " + res);
+        int delete_id = 21;
+        res = userMapper.delete_user_by_ID(delete_id);
+        System.out.println("删除的数量: " + res);
 
+        int[] delete_ids = {5, 6, 7, 8};
+        res = userMapper.delete_user_by_IDS(delete_ids);
+        System.out.println("批量删除的数量: " + res);
+
+        user1.setPassword("update");
+        res = userMapper.update_user(user1);
+        System.out.println("修改的数量: " + res);
+
+        // Tenant
         TenantMapper tenantMapper = sqlSession.getMapper(TenantMapper.class);
         List<Tenant> tenants = tenantMapper.select_all_tenants();
         System.out.println(tenants);
--- src/main/java/com/example/demo1/mapper/UserMapper.java
+++ src/main/java/com/example/demo1/mapper/UserMapper.java
@@ -1,14 +1,25 @@
 package com.example.demo1.mapper;
 
 import com.example.demo1.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface UserMapper {
+    int insert_user(User user);
+
+    int delete_user_by_ID(int ID);
+
+    int delete_user_by_IDS(int[] IDS); // 这个的命名无所谓
+
+    int update_user(User user);
+
     public List<User> select_all_users();
-    public User select_user_by_id(int id);
+    public User select_user_by_ID(int ID);
     public default User select_user(User user){
-        return select_user_by_id(user.getID());
+        return select_user_by_ID(user.getID());
     }
     public User select_user_by_Name(String Name);
+
+    public User select_user_by_Name_and_Password(@Param("Name") String Name, @Param("Password") String Password);
 }
