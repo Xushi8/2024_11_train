1db282e2a992bc7fadd9e9dc0b87021d41245a9c
uuufa <111222uuufa@gmail.com>
2024-11-19 15:36:25 +0800

    修改数据库对应的表

--- src/main/java/com/example/demo1/mapper/ProjectUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo1.mapper;
-
-import com.example.demo1.pojo.ProjectUser;
-
-import java.util.List;
-
-public interface ProjectUserMapper {
-    public List<ProjectUser> select_all_project_users();
-}
