db88cb9d4fa85587c7c0f7611451c2fcdf1b8d91
uuufa <111222uuufa@gmail.com>
2024-11-20 15:41:54 +0800

    tenant requestBody

--- src/main/java/com/example/demo1/controller/TenantController.java
+++ src/main/java/com/example/demo1/controller/TenantController.java
@@ -6,6 +6,7 @@ import com.example.demo1.service.TenantService;
 import com.example.demo1.service.TenantServiceImpl;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,21 +18,21 @@ public class TenantController {
     private TenantServiceImpl tenantServiceImpl;
 
     @RequestMapping("/insert_tenant")
-    public int insert_tenant(Tenant tenant){
+    public int insert_tenant(@RequestBody Tenant tenant){
         return tenantServiceImpl.insert_tenant(tenant);
     }
 
     @RequestMapping("/delete_tenant_by_id")
-    public int delete_tenant_by_id(int id){
+    public int delete_tenant_by_id(@RequestBody int id){
         return tenantServiceImpl.delete_tenant_by_id(id);
     }
     @RequestMapping("/delete_tenants_by_ids")
-    public int delete_tenants_by_ids(int[] ids){
+    public int delete_tenants_by_ids(@RequestBody int[] ids){
         return tenantServiceImpl.delete_tenants_by_ids(ids);
     }
 
     @RequestMapping("/update_tenant")
-    public int update_tenant(Tenant tenant){
+    public int update_tenant(@RequestBody Tenant tenant){
         return tenantServiceImpl.update_tenant(tenant);
     }
 
@@ -40,15 +41,15 @@ public class TenantController {
         return tenantServiceImpl.select_all_tenants();
     }
     @RequestMapping("/select_tenant_by_id")
-    public Tenant select_tenant_by_id(int id){
+    public Tenant select_tenant_by_id(@RequestBody int id){
         return tenantServiceImpl.select_tenant_by_id(id);
     }
     @RequestMapping("/select_tenant_by_name")
-    public Tenant select_tenant_by_name(@Param("name") String name){
+    public Tenant select_tenant_by_name(@RequestBody String name){
         return tenantServiceImpl.select_tenant_by_name(name);
     }
     @RequestMapping("/select_tenant_by_name_and_password")
-    public Tenant select_tenant_by_name_and_password(@Param("name") String name, @Param("password") String password){
+    public Tenant select_tenant_by_name_and_password(@RequestBody String name, @RequestBody String password){
         return tenantServiceImpl.select_tenant_by_name_and_password(name, password);
     }
 }
